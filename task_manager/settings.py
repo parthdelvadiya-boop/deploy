27fc935ffc418c5e158505d583d4488954df208c
NIghtshift <nightshift@Kartik-Viradiyas-Mac-mini.local>
2026-08-13 02:11:19 +0530

    done

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
+if os.environ.get("DATABASE_URL"):
+    import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
+
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
